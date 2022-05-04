41d3ada51e55014a67308bf90a1d38afda2ed7d4
GraveofBears <modf999@yahoo.com>
2022-05-04 14:53:47 -0400

    Add project files.

---
+++ OdinsHollow.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using ItemManager;
+using UnityEngine;
+using PieceManager;
+using ServerSync;
+using HarmonyLib;
+
+
+namespace OdinsHollow
+{
+	[BepInPlugin(ModGUID, ModName, ModVersion)]
+	public class OdinsHollow : BaseUnityPlugin
+	{
+		private const string ModName = "OdinsHollow";
+		private const string ModVersion = "1.0.0";
+		private const string ModGUID = "org.bepinex.plugins.odinshollow";
+
+
+		public void Awake()
+		{
+			Item OdinsHollowWand = new("odinshollow", "OdinsHollowWand");
+			OdinsHollowWand.Crafting.Add(CraftingTable.Workbench, 10);
+			OdinsHollowWand.RequiredItems.Add("SwordCheat", 1);
+			OdinsHollowWand.CraftAmount = 1;
+
+
+			GameObject OH_Cave_Hall_1 = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_Cave_Hall_1");
+			GameObject OH_Cave_Hall_2 = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_Cave_Hall_2");
+			GameObject OH_Cave_Room_1 = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_Cave_Room_1");
+			GameObject OH_Cave_Room_2 = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_Cave_Room_2");
+			GameObject OH_Cave_Room_3 = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_Cave_Room_3");
+			GameObject OH_Cave_End_1 = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_Cave_End_1");
+			GameObject OH_Cave_End_2 = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_Cave_End_2");
+			GameObject OH_Cave_End_3 = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_Cave_End_3");
+			GameObject OH_OdinsHollow = ItemManager.PrefabManager.RegisterPrefab("odinshollow", "OH_OdinsHollow");
+
+			new Harmony(ModName).PatchAll();
+
+
+		}
+	}
+
+}
