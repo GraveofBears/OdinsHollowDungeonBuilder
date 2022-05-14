c605664ac5ac819c2e6ef7c113d324a3db3616fc
GraveofBears <modf999@yahoo.com>
2022-05-14 11:15:59 -0400

    thanks blaxxun, sorry I'm shit

--- OdinsHollow.cs
+++ OdinsHollow.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using BepInEx;
+using BepInEx.Configuration;
 using ItemManager;
 using UnityEngine;
 using LocationManager;
 using PieceManager;
 using ServerSync;
 using HarmonyLib;
+using System.Reflection;
 
 namespace OdinsHollow
 {
@@ -15,13 +19,40 @@ namespace OdinsHollow
 		private const string ModName = "OdinsHollow";
 		private const string ModVersion = "1.0.2";
 		private const string ModGUID = "org.bepinex.plugins.odinshollow";
+		private static Harmony harmony = null!;
 
+		private static readonly Dictionary<BuildPiece, ConfigEntry<string>> CreaturesInSpawners = new();
 
-        public void Awake()
+		private readonly ConfigSync configSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+		private static ConfigEntry<bool> ServerConfigLocked = null!;
+		private static ConfigEntry<string> OH_Spawner_Shroom_1_Prefab = null!;
+		private static ConfigEntry<string> OH_Spawner_Shroom_2_Prefab = null!;
+		private static ConfigEntry<string> OH_Spawner_Shroom_3_Prefab = null!;
+		private static ConfigEntry<string> OH_Spawner_Shroom_4_Prefab = null!;
+
+		private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
 		{
+			ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
 
+			SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+			syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
 
-		Item OdinsHollowWand = new("odinshollow", "OdinsHollowWand");
+			return configEntry;
+		}
+
+		private ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+
+		public void Awake()
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			harmony = new Harmony(ModGUID);
+			harmony.PatchAll(assembly);
+
+			ServerConfigLocked = config("1 - General", "Lock Configuration", true, "If on, the configuration is locked and can be changed by server admins only.");
+			configSync.AddLockingConfigEntry(ServerConfigLocked);
+
+			Item OdinsHollowWand = new("odinshollow", "OdinsHollowWand");
 			OdinsHollowWand.Crafting.Add(CraftingTable.StoneCutter, 15);
 			OdinsHollowWand.RequiredItems.Add("SwordCheat", 1);
 			OdinsHollowWand.CraftAmount = 1;
@@ -43,36 +74,37 @@ namespace OdinsHollow
 
 			BuildPiece OH_Spawner_Shroom_1 = new(PiecePrefabManager.RegisterAssetBundle("odinshollow"), "OH_Spawner_Shroom_1", true, "OdinsHollowWand");
 			OH_Spawner_Shroom_1.Name.English("OH Spawner Shroom 1");
-			OH_Spawner_Shroom_1.Description.English("A Dungeon Spawner1 Shroom");
+			OH_Spawner_Shroom_1.Description.English("A Dungeon Spawner Shroom");
 			OH_Spawner_Shroom_1.RequiredItems.Add("SwordCheat", 1, false);
 
 			BuildPiece OH_Spawner_Shroom_2 = new(PiecePrefabManager.RegisterAssetBundle("odinshollow"), "OH_Spawner_Shroom_2", true, "OdinsHollowWand");
 			OH_Spawner_Shroom_2.Name.English("OH Spawner Shroom 2");
-			OH_Spawner_Shroom_2.Description.English("A Dungeon Spawner2 Shroom");
+			OH_Spawner_Shroom_2.Description.English("A Dungeon Spawner Shroom");
 			OH_Spawner_Shroom_2.RequiredItems.Add("SwordCheat", 1, false);
 
 			BuildPiece OH_Spawner_Shroom_3 = new(PiecePrefabManager.RegisterAssetBundle("odinshollow"), "OH_Spawner_Shroom_3", true, "OdinsHollowWand");
 			OH_Spawner_Shroom_3.Name.English("OH Spawner Shroom 3");
-			OH_Spawner_Shroom_3.Description.English("A Dungeon Spawner3 Shroom");
+			OH_Spawner_Shroom_3.Description.English("A Dungeon Spawner Shroom");
 			OH_Spawner_Shroom_3.RequiredItems.Add("SwordCheat", 1, false);
 
 			BuildPiece OH_Spawner_Shroom_4 = new(PiecePrefabManager.RegisterAssetBundle("odinshollow"), "OH_Spawner_Shroom_4", true, "OdinsHollowWand");
 			OH_Spawner_Shroom_4.Name.English("OH Spawner Shroom 4");
-			OH_Spawner_Shroom_4.Description.English("A Dungeon Spawner4 Shroom");
+			OH_Spawner_Shroom_4.Description.English("A Dungeon Spawner Shroom");
 			OH_Spawner_Shroom_4.RequiredItems.Add("SwordCheat", 1, false);
 
-			BuildPiece OH_Spawner_Shroom_5 = new(PiecePrefabManager.RegisterAssetBundle("odinshollow"), "OH_Spawner_Shroom_5", true, "OdinsHollowWand");
-			OH_Spawner_Shroom_5.Name.English("OH Spawner Shroom 5");
-			OH_Spawner_Shroom_5.Description.English("A Dungeon Spawner5 Shroom");
-			OH_Spawner_Shroom_5.RequiredItems.Add("SwordCheat", 1, false);
+			OH_Spawner_Shroom_1_Prefab = config("2 - Spawner", "Creature for Shroom spawner 1", "Neck", "Prefab name for the creature that should spawn at the first shroom spawner.");
+			OH_Spawner_Shroom_1_Prefab.SettingChanged += (_, _) => UpdateSpawner(OH_Spawner_Shroom_1, OH_Spawner_Shroom_1_Prefab);
+			OH_Spawner_Shroom_2_Prefab = config("2 - Spawner", "Creature for Shroom spawner 2", "Boar", "Prefab name for the creature that should spawn at the second shroom spawner.");
+			OH_Spawner_Shroom_2_Prefab.SettingChanged += (_, _) => UpdateSpawner(OH_Spawner_Shroom_2, OH_Spawner_Shroom_2_Prefab);
+			OH_Spawner_Shroom_3_Prefab = config("2 - Spawner", "Creature for Shroom spawner 3", "Greyling", "Prefab name for the creature that should spawn at the third shroom spawner.");
+			OH_Spawner_Shroom_3_Prefab.SettingChanged += (_, _) => UpdateSpawner(OH_Spawner_Shroom_3, OH_Spawner_Shroom_3_Prefab);
+			OH_Spawner_Shroom_4_Prefab = config("2 - Spawner", "Creature for Shroom spawner 4", "Bat", "Prefab name for the creature that should spawn at the fourth shroom spawner.");
+			OH_Spawner_Shroom_4_Prefab.SettingChanged += (_, _) => UpdateSpawner(OH_Spawner_Shroom_4, OH_Spawner_Shroom_4_Prefab);
 
-			CreaturesInSpawners.Add(OH_Spawner_Shroom_1, "Boar");
-			CreaturesInSpawners.Add(OH_Spawner_Shroom_2, "Boar");
-			CreaturesInSpawners.Add(OH_Spawner_Shroom_3, "Greyling");
-			CreaturesInSpawners.Add(OH_Spawner_Shroom_4, "Bat");
-			CreaturesInSpawners.Add(OH_Spawner_Shroom_5, "Skeleton");
-
-
+			CreaturesInSpawners.Add(OH_Spawner_Shroom_1, OH_Spawner_Shroom_1_Prefab);
+			CreaturesInSpawners.Add(OH_Spawner_Shroom_2, OH_Spawner_Shroom_2_Prefab);
+			CreaturesInSpawners.Add(OH_Spawner_Shroom_3, OH_Spawner_Shroom_3_Prefab);
+			CreaturesInSpawners.Add(OH_Spawner_Shroom_4, OH_Spawner_Shroom_4_Prefab);
 
 			_ = new LocationManager.Location("odinshollow", "OdinsHollowDungeon")
 			{
@@ -85,24 +117,36 @@ namespace OdinsHollow
 				MinimumDistanceFromGroup = 100,
 				Count = 15,
 				Unique = true
-
 			};
 
 			new Harmony(ModName).PatchAll();
 		}
 
-		private static readonly Dictionary<BuildPiece, string> CreaturesInSpawners = new();
+		private static void UpdateSpawner(BuildPiece piece, ConfigEntry<string> configEntry)
+		{
+			if (ZNetScene.instance.GetPrefab(configEntry.Value) is { } character)
+			{
+				if (Resources.FindObjectsOfTypeAll<CreatureSpawner>().FirstOrDefault(c => piece.Prefab.GetComponentInChildren<CreatureSpawner>().name.StartsWith(c.name, StringComparison.Ordinal)) is { } creatureSpawner)
+				{
+					creatureSpawner.m_creaturePrefab = character;
+				}
+				piece.Prefab.transform.GetComponentInChildren<CreatureSpawner>().m_creaturePrefab = character;
+			}
+		}
 
 		[HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
 		private class AddCreatureToSpawner
 		{
 			private static void Postfix(ZNetScene __instance)
 			{
-				foreach (KeyValuePair<BuildPiece, string> kv in CreaturesInSpawners)
+				foreach (KeyValuePair<BuildPiece, ConfigEntry<string>> kv in CreaturesInSpawners)
 				{
-					foreach (CreatureSpawner spawner in kv.Key.Prefab.transform.GetComponentsInChildren<CreatureSpawner>())
+					if (__instance.GetPrefab(kv.Value.Value) is { } character)
 					{
-						spawner.m_creaturePrefab = __instance.GetPrefab(kv.Value);
+						foreach (CreatureSpawner spawner in kv.Key.Prefab.transform.GetComponentsInChildren<CreatureSpawner>())
+						{
+							spawner.m_creaturePrefab = character;
+						}
 					}
 				}
 			}
